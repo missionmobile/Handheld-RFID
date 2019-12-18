f6fcb2cfdd46785b45aab8a5c4266d210884fe04
Gunnar <g.lehker@gmail.com>
2019-12-18 15:18:25 +0100

    Update SearchTagActivity.java
    call callback success and end class

--- src/android/SearchTagActivity.java
+++ src/android/SearchTagActivity.java
@@ -1,9 +1,5 @@
 package de.mindsquare.rfid;
 
-import android.annotation.SuppressLint;
-import android.media.AudioManager;
-import android.media.ToneGenerator;
-import android.os.Bundle;
 import android.util.Log;
 
 import org.apache.cordova.CallbackContext;
@@ -13,9 +9,9 @@ import java.io.IOException;
 public class SearchTagActivity extends SerialPortActivity {
     private static final String TAG = SearchTagActivity.class.getSimpleName();
 
-    private boolean mByteReceivedBack;
-    private final Object mByteReceivedBackSemaphore = new Object();
-    private String mTagType, mIDBitCount, mID;
+    boolean mByteReceivedBack;
+    final Object mByteReceivedBackSemaphore = new Object();
+    String mTagType, mIDBitCount, mID;
     Integer cmd;
 
     SendingThread mSendingThread;
@@ -23,16 +19,18 @@ public class SearchTagActivity extends SerialPortActivity {
     byte[] rxbuffer_raw = new byte[200];
     byte[] rxbuffer = new byte[200];
     int rxbuffer_Index;
-    //ToneGenerator toneGen1 = new ToneGenerator(AudioManager.STREAM_RING, 80);
 
-    SearchTagActivity(CallbackContext cbCtx){
+    CallbackContext cbCtx;
+
+    public SearchTagActivity(CallbackContext cbCtx){
         super(cbCtx);
 
+        this.cbCtx = cbCtx;
+
         if (mSerialPort != null) {
-            mSendingThread = new SendingThread();
+            mSendingThread = new SendingThread(this.cbCtx);
             mSendingThread.start();
         }
-
         mTagType = null;
         mIDBitCount = null;
         mID = null;
@@ -40,10 +38,14 @@ public class SearchTagActivity extends SerialPortActivity {
     }
 
     private class SendingThread extends Thread {
+
+        CallbackContext cbCtx;
+        public SendingThread(CallbackContext cbCtx){
+            this.cbCtx = cbCtx;
+        }
+
         @Override
         public void run() {
-            super.run();
-
             while (!isInterrupted()) {
                 synchronized (mByteReceivedBackSemaphore) {
                     mByteReceivedBack = false;
@@ -51,6 +53,7 @@ public class SearchTagActivity extends SerialPortActivity {
                         if (mOutputStream != null) {
                             if (cmd == 1) {
                                 rxbuffer_Index = 0;
+                                txbuffer = new byte[50];
                                 txbuffer[0] = '0';
                                 txbuffer[1] = '5';
                                 txbuffer[2] = '0';
@@ -62,7 +65,9 @@ public class SearchTagActivity extends SerialPortActivity {
                                 Log.d(TAG, "SearchTag-GetTagType");
                                 //cmd=0;
                             } else if (cmd == 2) { // SetTagType only LF
+                                // TODO: Make sure all LF tags are enabled
                                 rxbuffer_Index = 0;
+                                txbuffer = new byte[50];
                                 txbuffer[0] = '0';
                                 txbuffer[1] = '5';
                                 txbuffer[2] = '0';
@@ -96,16 +101,16 @@ public class SearchTagActivity extends SerialPortActivity {
                             else
                                 cmd = 2;
                         }
+
                     } catch (InterruptedException ignored) {
                     }
                 }
             }
-            Log.d(TAG, "Stopped Thread!");
         }
     }
 
-    @SuppressLint("DefaultLocale")
-    protected void onDataReceived(byte[] buffer, int size, CallbackContext cbCtx) {
+    @Override
+    protected void onDataReceived(byte[] buffer, int size, Thread t) {
 
         synchronized (mByteReceivedBackSemaphore) {
 
@@ -113,15 +118,6 @@ public class SearchTagActivity extends SerialPortActivity {
             int temp1, temp2;
             for (i = 0; i < size; i++)                    // read the bytes and save them
             {
-                if(rxbuffer_Index >= rxbuffer_raw.length) {
-                    // Shift array left
-                    for(int j=0; j<rxbuffer_raw.length / 2; j++) {
-                        rxbuffer_raw[j] = rxbuffer_raw[(rxbuffer_raw.length / 2) + j];
-                        rxbuffer_raw[(rxbuffer_raw.length / 2) + j] = 0;
-                        Log.d(TAG, "Shortened Array");
-                        rxbuffer_Index = rxbuffer_raw.length / 2;
-                    }
-                }
                 rxbuffer_raw[rxbuffer_Index++] = buffer[i];
             }
             for (i = 0; i < rxbuffer_Index / 2; i++)        // transfer two raw bytes to one byte
@@ -130,77 +126,68 @@ public class SearchTagActivity extends SerialPortActivity {
                 temp2 = (rxbuffer_raw[i * 2 + 1] >= 65) ? (rxbuffer_raw[i * 2 + 1] - 55) : (rxbuffer_raw[i * 2 + 1] - 48);
                 rxbuffer[i] = (byte) ((temp1 << 4) + temp2);
             }
-            rxbuffer = rxbuffer_raw;
-            for(i=0; i < rxbuffer.length - 9; i++) {
-                if (rxbuffer[i] == 0x01 && rxbuffer[i+1] == 0x040 && rxbuffer[i+4] != (byte) 0  && rxbuffer[i+5] != (byte) 0  && rxbuffer[i+6] != (byte) 0 && rxbuffer[i+7] != (byte) 0) {                          // transponder found
-                    int type;
-                    type = rxbuffer[i+1];
-                    if (type == 0x040)
-                        mTagType = "EM4x02/CASI-RUSCO";
-                    else if (type == 0x041)
-                        mTagType = "HITAG 1/HITAG S";
-                    else if (type == 0x042)
-                        mTagType = "HITAG 2";
-                    else if (type == 0x043)
-                        mTagType = "EM4x50";
-                    else if (type == 0x044)
-                        mTagType = "T55x7";
-                    else if (type == 0x045)
-                        mTagType = "ISO FDX-B";
-                    else if (type == 0x046)
-                        mTagType = "N/A";
-                    else if (type == 0x047)
-                        mTagType = "N/A";
-                    else if (type == 0x048)
-                        mTagType = "N/A";
-                    else if (type == 0x049)
-                        mTagType = "HID Prox";
-                    else if (type == 0x04A)
-                        mTagType = "ISO HDX/TIRIS";
-                    else if (type == 0x04B)
-                        mTagType = "Cotag";
-                    else if (type == 0x04C)
-                        mTagType = "ioProx";
-                    else if (type == 0x04D)
-                        mTagType = "Indala";
-                    else if (type == 0x04E)
-                        mTagType = "NexWatch";
-                    else if (type == 0x04F)
-                        mTagType = "AWID";
-                    else if (type == 0x050)
-                        mTagType = "G-Prox";
-                    else if (type == 0x051)
-                        mTagType = "Pyramid";
-                    else if (type == 0x052)
-                        mTagType = "Keri";
-                    else if (type == 0x053)
-                        mTagType = "N/A";
-                    else
-                        mTagType = String.format("%02X Hex", rxbuffer[i+1]);
-                    mIDBitCount = String.format("%d Bits", rxbuffer[i+2]);
-                    mID = String.format("%02X %02X %02X %02X", rxbuffer[i+4], rxbuffer[i+5], rxbuffer[i+6], rxbuffer[i+7]);
-                    if (!mSendingThread.isInterrupted()) {
-                        Log.d(TAG, mID);
-                        cbCtx.success(mID);
-                        mSendingThread.interrupt();
-                    }
-                    // short Tone
-                    //toneGen1.startTone(ToneGenerator.TONE_PROP_ACK, 100);
-                } else { // there is no transponder
-                    //cbCtx.success(mID);
-                    //Log.d(TAG, "Found no transponder: ");
-                    //mSendingThread.interrupt();
+            if (rxbuffer[1] == 0x01) {                            // transponder found
+                int type;
+                type = rxbuffer[2];
+                if (type == 0x040)
+                    mTagType = "EM4x02/CASI-RUSCO";
+                else if (type == 0x041)
+                    mTagType = "HITAG 1/HITAG S";
+                else if (type == 0x042)
+                    mTagType = "HITAG 2";
+                else if (type == 0x043)
+                    mTagType = "EM4x50";
+                else if (type == 0x044)
+                    mTagType = "T55x7";
+                else if (type == 0x045)
+                    mTagType = "ISO FDX-B";
+                else if (type == 0x046)
+                    mTagType = "N/A";
+                else if (type == 0x047)
+                    mTagType = "N/A";
+                else if (type == 0x048)
+                    mTagType = "N/A";
+                else if (type == 0x049)
+                    mTagType = "HID Prox";
+                else if (type == 0x04A)
+                    mTagType = "ISO HDX/TIRIS";
+                else if (type == 0x04B)
+                    mTagType = "Cotag";
+                else if (type == 0x04C)
+                    mTagType = "ioProx";
+                else if (type == 0x04D)
+                    mTagType = "Indala";
+                else if (type == 0x04E)
+                    mTagType = "NexWatch";
+                else if (type == 0x04F)
+                    mTagType = "AWID";
+                else if (type == 0x050)
+                    mTagType = "G-Prox";
+                else if (type == 0x051)
+                    mTagType = "Pyramid";
+                else if (type == 0x052)
+                    mTagType = "Keri";
+                else if (type == 0x053)
+                    mTagType = "N/A";
+                else
+                    mTagType = String.format("%02X Hex", rxbuffer[2]);
+
+                mIDBitCount = String.format("%d", rxbuffer[3]);
+                mID = String.format("%02X %02X %02X %02X", rxbuffer[5], rxbuffer[6], rxbuffer[7], rxbuffer[8]);
+
+                if(rxbuffer[5] != 0 && rxbuffer[6] != 0 && rxbuffer[7] != 0 && rxbuffer[8] != 0){
+                    this.cbCtx.success(mID);
+                    t.interrupt();
+                    mSendingThread.interrupt();
                 }
             }
         }
     }
 
-    /*
     @Override
     protected void onDestroy() {
         if (mSendingThread != null)
             mSendingThread.interrupt();
         super.onDestroy();
-    }*/
+    }
 }
-
