477c1bb5b31d0d629ef4183dc71bb735631292b3
Tim Kostka <kostka@mindsquare.de>
2021-01-06 13:41:53 +0000

    test with https://www.handheldgroup.com/knowledge-base/accessing-the-serial-port-on-the-back-connector/

--- src/android/SerialPortActivity.java
+++ src/android/SerialPortActivity.java
@@ -1,26 +1,5 @@
 package de.mindsquare.rfid;
 
-
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.os.SystemClock;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-
-import com.handheldgroup.serialport.SerialPort;
-// import com.lovdream.ILovdreamDevice;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.InvalidParameterException;
-
-/*
 import android.app.Activity;
 import android.os.SystemClock;
 import android.os.Build;
@@ -35,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.security.InvalidParameterException;
-*/
 
 public abstract class SerialPortActivity extends Activity {
     protected SerialPort mSerialPort;
