a711b0dcf889071e082fe7ebbb4293cbebd223ee
Tim Kostka <kostka@mindsquare.de>
2021-01-06 14:52:08 +0000

    add sources

--- src/android/SerialPortActivity.java
+++ src/android/SerialPortActivity.java
@@ -4,6 +4,16 @@ import android.app.Activity;
 import android.os.SystemClock;
 import android.os.Build;
 
+//neu
+import android.os.Bundle;
+import android.os.IBinder;
+import android.os.SystemClock;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.content.DialogInterface;
+import android.content.Intent;
+
 import com.handheldgroup.serialport.SerialPort;
 import com.lovdream.ILovdreamDevice;
 
